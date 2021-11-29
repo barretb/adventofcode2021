78084805f5d89db3a2e39d43d2d6cd707a59578c
Barret <barretblake@live.com>
2021-11-29 15:24:17 -0500

    Setting up a sample template program.cs file

---
+++ AofC2021/TemplateApp/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Advent of Code 2021");
+Console.WriteLine("Day 1 - Puzzle 1");
+
+//Solution logic goes here
+
+
+
+
+
+
+
+
+//Stop and wait for enter before exiting
+Console.ReadLine();
