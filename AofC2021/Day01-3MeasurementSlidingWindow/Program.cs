9efa4d6f68a37d9fd630767fee082ca41bbafb57
Barret <barretblake@live.com>
2021-12-01 10:00:27 -0500

    Solutions for day 1

---
+++ AofC2021/Day01-3MeasurementSlidingWindow/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.WriteLine("Advent of Code 2021");
+Console.WriteLine("Day 1 - Sonar Sweep");
+
+//Solution logic goes here
+
+//Load input array
+string[] lines = System.IO.File.ReadAllLines("input.txt");
+int[] measurements = new int[lines.Length];
+for (int i = 0; i < lines.Length; i++)
+{
+      measurements[i] = int.Parse(lines[i]);
+}
+
+int depth1 = 0, depth2 = 0;
+var increases = 0;
+for (int i = 0; i < lines.Length - 3; i++)
+{
+    depth1 = measurements[i] + measurements[i + 1] + measurements[i + 2];
+    depth2 = measurements[i + 1] + measurements[i + 2] + measurements[i + 3];
+    if(depth2 > depth1) increases++;
+}
+
+//Write out the answer
+Console.WriteLine($"Number of increases: {increases}");
+
+//Stop and wait for enter before exiting
+Console.ReadLine();
---
+++ AofC2021/Day01-SonarSweep/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Advent of Code 2021");
+Console.WriteLine("Day 1 - Sonar Sweep");
+
+//Solution logic goes here
+
+//Load input array
+string[] lines = System.IO.File.ReadAllLines("input.txt");
+
+var depth = int.Parse(lines[0]);
+var increases = 0;
+for (int i = 1; i < lines.Length; i++)
+{
+    var measurement = int.Parse(lines[i]);
+    if (measurement > depth) increases++;
+    depth = measurement;
+}
+
+//Write out the solution
+Console.WriteLine($"Number of increases: {increases}");
+
+
+//Stop and wait for enter before exiting
+Console.ReadLine();
