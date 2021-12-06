19528f706f077d9cff956fae8f2e9381604cecbe
Barret <barretblake@live.com>
2021-12-06 12:03:24 -0500

    Day 6 solutions

---
+++ AofC2021/Day06-Puzzle1/Program.cs
@@ -0,0 +1,39 @@
+﻿Console.WriteLine("Advent of Code 2021");
+Console.WriteLine("Day 6 - Puzzle 1");
+
+//Solution logic goes here
+
+//Load input array
+string[] lines = System.IO.File.ReadAllLines("input.txt");
+
+var strLanternFish = lines[0].Split(',');
+var lanternFish = new List<int>();
+foreach (var fish in strLanternFish)
+{
+    lanternFish.Add(int.Parse(fish));
+}
+
+for (int x = 0; x < 80; x++)
+{
+    var newList = new List<int>();
+    foreach (var fishie in lanternFish)
+    {
+        if (fishie == 0)
+        {
+            //Time for a new fishie
+            newList.Add(8);
+            newList.Add(6);
+        }
+        else
+        {
+            //Just another day in the lifecycle
+            newList.Add(fishie - 1);
+        }
+    }
+    lanternFish = newList;
+}
+
+Console.WriteLine($"Total Fish after 80 days: {lanternFish.Count}");
+
+//Stop and wait for enter before exiting
+Console.ReadLine();
---
+++ AofC2021/Day06-Puzzle2-256Days/Program.cs
@@ -0,0 +1,48 @@
+﻿Console.WriteLine("Advent of Code 2021");
+Console.WriteLine("Day 6 - Puzzle 1");
+
+//Solution logic goes here
+
+//Load input array
+string[] lines = System.IO.File.ReadAllLines("input.txt");
+
+var strLanternFish = lines[0].Split(',');
+var lanternFish = new List<int>();
+foreach (var fish in strLanternFish)
+{
+    lanternFish.Add(int.Parse(fish));
+}
+
+//initialize number of fish
+var dictFish = new Dictionary<int, ulong>();
+for (int a = 0; a <= 8; a++)
+{
+    dictFish[a] = 0;
+}
+
+foreach (var item in lanternFish)
+{
+    dictFish[item]++;
+}
+
+for (int x = 0; x < 256; x++)
+{
+    var fishReproducing = dictFish[0];
+    for (int y = 0; y < 8; y++)
+    {
+        //shift all fish down one day
+        dictFish[y] = dictFish[y + 1];
+    }
+    //now reset reproducers
+    dictFish[6] = dictFish[6] + fishReproducing;
+    dictFish[8] = fishReproducing;
+}
+ulong fishcount = 0;
+for (var b = 0; b < 9; b++)
+{
+    fishcount += dictFish[b];
+}
+Console.WriteLine($"Total Fish after 256 days: {fishcount}");
+
+//Stop and wait for enter before exiting
+Console.ReadLine();
