78deb45012bc3b54d10705917946b4370f5da1d5
Barret <barretblake@live.com>
2021-12-10 13:09:26 -0500

    AOC Day 10

--- AofC2021/Day10-Puzzle1-NavigationSystem/Program.cs
+++ AofC2021/Day10-Puzzle1-NavigationSystem/Program.cs
@@ -11,7 +11,6 @@ var points = 0;
 for(int i = 0; i < lines.Length; i++)
 {
     var line = lines[i];
-    Console.WriteLine(line);
 
     //first, clean out all possible matches
     var foundMatches = true;
@@ -26,54 +25,23 @@ for(int i = 0; i < lines.Length; i++)
         line = line.Replace("()", "");
     }
 
-    Console.WriteLine(line);
     if (line.Contains(">") || line.Contains("]") || line.Contains("}") || line.Contains(")"))
     {
-        Console.WriteLine("CORRUPTED");
+        //CORRUPTED
         Match match = Regex.Match(line, @"[\]\}\)\>]", RegexOptions.None);
         if (match.Success)
         {
             var m = match.Groups[0].Value;
-            Console.WriteLine($"First match: {m}");
             if (m == ")") points += 3;
             if (m == "]") points += 57;
             if (m == "}") points += 1197;
             if (m == ">") points += 25137;            
         }
     }
-    else Console.WriteLine("INCOMPLETE");
-
-    Console.WriteLine();
-    Console.WriteLine();
+    //Otherwise it's incomplete. Do nothing for now
 }
 
 Console.WriteLine($"Total Points: {points}");
 
 //Stop and wait for enter before exiting
 Console.ReadLine();
-
-//static char CheckCharacter(char expected, string testString)
-//{
-//    if (testString.Length == 1) return testString[0];
-//    var first = testString[0];
-//    string remainder = testString.Substring(1, testString.Length - 1);
-//    if(first == '(')
-//    {
-//        if (CheckCharacter(first, remainder) != ')') throw new Exception("Corrupted");
-//    } else if (first == '[')
-//    {
-//        if (CheckCharacter(first, remainder) != ']') throw new Exception("Corrupted");
-//    }
-//    else if (first == '{')
-//    {
-//        if (CheckCharacter(first, remainder) != '}') throw new Exception("Corrupted");
-//    }
-//    else if (first == '<')
-//    {
-//        if (CheckCharacter(first, remainder) != '>') throw new Exception("Corrupted");
-//    } else
-//    {
-
-//    }
-
-//}
---
+++ AofC2021/Day10-Puzzle2-FixTheIncompleteLines/Program.cs
@@ -0,0 +1,72 @@
+﻿using System.Text;
+using System.Text.RegularExpressions;
+
+Console.WriteLine("Advent of Code 2021");
+Console.WriteLine("Day 10 - Puzzle 1");
+
+//Solution logic goes here
+
+//Load input array
+string[] lines = System.IO.File.ReadAllLines("input.txt");
+var points = new List<ulong>();
+for (int i = 0; i < lines.Length; i++)
+{
+    var line = lines[i];
+    var fixedline = new StringBuilder();
+    fixedline.Append(line);
+
+    //first, clean out all possible matches
+    var foundMatches = true;
+    while (foundMatches)
+    {
+        foundMatches = false;
+
+        if (line.Contains("<>") || line.Contains("[]") || line.Contains("{}") || line.Contains("()")) foundMatches = true;
+        line = line.Replace("<>", "");
+        line = line.Replace("[]", "");
+        line = line.Replace("{}", "");
+        line = line.Replace("()", "");
+    }
+
+    if (line.Contains(">") || line.Contains("]") || line.Contains("}") || line.Contains(")"))
+    {
+        //CORRUPTED - discard
+    }
+    else
+    {
+        ulong linepoints = 0;
+        //Otherwise it's incomplete. Fix the line
+        for (var j = line.Length - 1; j >= 0; j--)
+        {
+            switch (line[j])
+            {
+                case '(':
+                    fixedline.Append(")");
+                    linepoints = (linepoints * 5) + 1;
+                    break;
+                case '[':
+                    fixedline.Append("]");
+                    linepoints = (linepoints * 5) + 2;
+                    break;
+                case '{':
+                    fixedline.Append("}");
+                    linepoints = (linepoints * 5) + 3;
+                    break;
+                case '<':
+                    fixedline.Append(">");
+                    linepoints = (linepoints * 5) + 4;
+                    break;
+            }
+        }
+
+        points.Add(linepoints);
+    }
+}
+
+//Find the middle points
+var score = points.OrderBy(x => x).Skip((points.Count / 2)).First();
+
+Console.WriteLine($"Total Points: {score}");
+
+//Stop and wait for enter before exiting
+Console.ReadLine();
